b96bc0846a43c0b6303df383fe279daad9f97d19
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-05-31 16:25:26 +0530

    Update show page of habits to show all details and mark habit as done

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -7,9 +7,16 @@ class HabitsController < ApplicationController
     today = Date.today
   
     @today_checkins = HabitCheckin.where(habit_id: @habits.pluck(:id), date: today).index_by(&:habit_id)
+    
+    @checkins_by_date = HabitCheckin.all.group_by(&:habit_id).transform_values do |checkins|
+      checkins.map(&:date)
+    end
   end
 
   def show
+    @start_date = @habit.created_at.to_date
+    @end_date = Date.today
+    @checkins = @habit.habit_checkins.index_by(&:date)
   end
 
   def new
